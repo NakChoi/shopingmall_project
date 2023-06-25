36ad879d4b9dd59c1591aac3f93af9a650288280
NakChoi <nakchoi94@gmail.com>
2023-06-25 20:12:02 +0900

    # feat : SecurityConfiguration
    filterChain 수정
    corsConfiguration 구현
    CustomFilterConfigurer 구현

--- shopping_mall/src/main/java/com/project/shopping_mall/security/config/SecurityConfiguration.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/security/config/SecurityConfiguration.java
@@ -1,47 +1,94 @@
 package com.project.shopping_mall.security.config;
 
 
+import com.project.shopping_mall.security.filter.JwtAuthenticationFilter;
+import com.project.shopping_mall.security.filter.JwtVerificationFilter;
+import com.project.shopping_mall.security.handler.MemberAuthenticationFailureHandler;
+import com.project.shopping_mall.security.handler.MemberAuthenticationSuccessHandler;
+import com.project.shopping_mall.security.jwt.JwtTokenizer;
+import com.project.shopping_mall.security.utils.CustomAuthorityUtils;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable()
-                .formLogin()
-                .loginPage("/auth/login-form")
-                .loginProcessingUrl("/process_login")
-                .failureUrl("/auths/login-form?error")
+                .headers().frameOptions().sameOrigin()
                 .and()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll();
+                .csrf().disable()
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 션을 생성하지 않도록 설정
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.POST, "/member").permitAll()
+                        .antMatchers(HttpMethod.PATCH, "/member/**").hasRole("USER")
+                        .antMatchers(HttpMethod.GET, "/member/**").hasRole("USER")
+                        .anyRequest().permitAll()
+                );
 
         return httpSecurity.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
-    public UserDetailsManager userDetailsManager() {
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
 
-        UserDetails userDetails =
-                User.withDefaultPasswordEncoder()
-                        .username("test1234@gmail.com")
-                        .password("test1234")
-                        .roles("USER")
-                        .build();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
 
-        return new InMemoryUserDetailsManager(userDetails);
+        return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception{
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class); // getSharedObject()를 통해서 Spring Security 의 설정을 구성하는 SecurityConfigurer 간에 공유되는 객체를 얻을 수 있다.
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager,jwtTokenizer ); //  JwtAuthenticationFilter를 생성하면서 JwtAuthenticationFilter에서 사용되는 AuthenticationManager와 JwtTokenizer를 DI 해준다.
+            jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class); // JwtVerificationFilter를 JwtAuthenticationFilter 뒤에 추가
+        }
     }
 
 }
