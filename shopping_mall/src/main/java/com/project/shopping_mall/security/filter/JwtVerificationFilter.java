c86d8b847006ef46742e981d76b404f53a2008e7
NakChoi <nakchoi94@gmail.com>
2023-06-25 20:10:26 +0900

    # feat : JwtVerification Filter  구현

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/security/filter/JwtVerificationFilter.java
@@ -0,0 +1,59 @@
+package com.project.shopping_mall.security.filter;
+
+import com.project.shopping_mall.security.jwt.JwtTokenizer;
+import com.project.shopping_mall.security.utils.CustomAuthorityUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+@AllArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils customAuthorityUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Map<String, Object> claims = verifyJws(request);
+        setAuthenticationToContext(claims);
+
+        filterChain.doFilter(request, response);
+    }
+
+
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String authorization = request.getHeader("Authorization");
+
+        return authorization == null || !authorization.startsWith("Bearer");
+    }
+
+    private Map<String, Object> verifyJws(HttpServletRequest request) {
+        String jws = request.getHeader("Authorization").replace("Bearer ", "");
+        String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
+        Map<String, Object> claims = jwtTokenizer.getClaims(jws, base64EncodedSecretKey).getBody(); // verify() 같은 검증 메서드가 따로 존재하는 것이 아니라 Claims가 정상적으로 파싱이 되면 서명 검증 역시 자연스럽게 성공했다는 뜻이다.
+
+        return claims;
+    }
+
+    private void setAuthenticationToContext(Map<String, Object> claims) {
+        String username = (String) claims.get("username");
+        List<GrantedAuthority> authorities = customAuthorityUtils.createAuthorities((List) claims.get("roles"));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+}
