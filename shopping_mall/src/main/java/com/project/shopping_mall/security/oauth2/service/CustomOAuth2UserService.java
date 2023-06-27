041b0f573a2cb15633e80942bb3f522f474354c1
NakChoi <nakchoi94@gmail.com>
2023-06-27 15:06:30 +0900

    # fix : PrincipaDetials 캡슐화

--- shopping_mall/src/main/java/com/project/shopping_mall/security/oauth2/service/CustomOAuth2UserService.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/security/oauth2/service/CustomOAuth2UserService.java
@@ -1,13 +1,15 @@
-package com.project.shopping_mall.security.service;
+package com.project.shopping_mall.security.oauth2.service;
 
 import com.project.shopping_mall.domain.member.entity.Member;
 import com.project.shopping_mall.domain.member.repository.MemberRepository;
 import com.project.shopping_mall.exception.CustomException;
 import com.project.shopping_mall.exception.ExceptionCode;
-import com.project.shopping_mall.security.oauth2.service.PrincipalDetails;
 import com.project.shopping_mall.security.oauth2.attribute.OAuth2Attributes;
 import com.project.shopping_mall.security.utils.CustomAuthorityUtils;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -15,8 +17,11 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -34,19 +39,15 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration()
                 .getProviderDetails()
                 .getUserInfoEndpoint()
-                .getUserNameAttributeName();
+                .getUserNameAttributeName(); // 어떤 소셜서비스든 그 서비스에서 각 계정마다의 유니크한 id값을 전달해주는 기능이다.(소셜 서비스 마다 유니크 필드명이 다르기 때문이다.)
 
 
-
-        OAuth2Attributes attributes = OAuth2Attributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-
+        OAuth2Attributes attributes = OAuth2Attributes.of(userNameAttributeName, oAuth2User.getAttributes());
 
         verifyOAuth2MemberByEmail(attributes.getEmail());
-
 
         Member verifiedMember = save(attributes);
 
@@ -83,4 +84,77 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         }
 
     }
+
+    private final class PrincipalDetails implements UserDetails, OAuth2User {
+        private Member member;
+        private Map<String, Object> attributes;
+
+        public PrincipalDetails(Member member){
+            this.member = member;
+        }
+
+        public PrincipalDetails(Member member, Map<String, Object> attributes){
+            this.member = member;
+            this.attributes = attributes;
+        }
+        @Override
+        public String getPassword() {
+            return member.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return member.getEmail();
+        }
+
+        // 해당 유저의 권한을 리턴하는 method
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            List<GrantedAuthority> authorities = this.member.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_"+ role))
+                    .collect(Collectors.toList());
+
+            return authorities;
+        }
+
+        @Override
+        // 만료 여부
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        // 잠금 여부
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        // 만료 여부
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        // 회원 사용 여부
+        public boolean isEnabled() {
+            return true;
+        }
+
+        @Override
+        public Map<String, Object> getAttributes() {
+            return attributes;
+        }
+
+        @Override
+        public <A> A getAttribute(String name) {
+            return OAuth2User.super.getAttribute(name);
+        }
+
+        @Override
+        public String getName() {
+            return member.getEmail();
+        }
+    }
+
 }
