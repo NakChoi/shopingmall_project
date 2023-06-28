5241f180aa163a118b9096ade6420941013ad909
NakChoi <nakchoi94@gmail.com>
2023-06-28 14:50:19 +0900

    # feat : RefreshTokenRepository 구현

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/redis/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.project.shopping_mall.redis.repository;
+
+import com.project.shopping_mall.redis.Entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByAccessToken(String accessToken);
+}
