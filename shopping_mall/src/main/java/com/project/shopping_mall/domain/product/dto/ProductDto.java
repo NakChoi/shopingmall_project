8c4b1d2334a5e5061508827373474c240186aabf
NakChoi <nakchoi94@gmail.com>
2023-07-04 19:51:53 +0900

    # feat : ProductDto 구현

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/dto/ProductDto.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/dto/ProductDto.java
@@ -1,4 +1,59 @@
 package com.project.shopping_mall.domain.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 public class ProductDto {
+
+
+    @Getter
+    public static class Post{
+
+        @NotBlank
+        private String name;
+
+        @NotBlank
+        private String description;
+
+        @NotBlank
+        private String price;
+    }
+
+    @Getter
+    public static class Patch{
+
+        private String name;
+
+        private String description;
+
+        private String price;
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response{
+
+        private String name;
+
+        private String description;
+
+        private String price;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PatchResponse{
+
+        private String name;
+
+        private String description;
+
+        private String price;
+    }
+
+
 }
