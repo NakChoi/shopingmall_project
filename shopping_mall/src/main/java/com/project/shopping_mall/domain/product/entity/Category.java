a16ab29af69ef27d16232b0fb4c8212c6c7bdf5f
NakChoi <nakchoi94@gmail.com>
2023-07-06 16:31:49 +0900

    # feat : Category Entity 구현

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/entity/Category.java
@@ -0,0 +1,27 @@
+package com.project.shopping_mall.domain.product.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    private String name;
+
+    private Integer seq;
+
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<ProductCategory> productCategories = new ArrayList<>();
+
+}
