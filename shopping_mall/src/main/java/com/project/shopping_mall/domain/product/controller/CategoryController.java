b7a6fd681f61d1412e8776b4ab9690759baa0866
NakChoi <nakchoi94@gmail.com>
2023-07-06 16:31:28 +0900

    # feat : Category Controller 구현

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/controller/CategoryController.java
@@ -0,0 +1,60 @@
+package com.project.shopping_mall.domain.product.controller;
+
+
+import com.project.shopping_mall.domain.product.dto.CategoryDto;
+import com.project.shopping_mall.domain.product.entity.Category;
+import com.project.shopping_mall.domain.product.entity.Product;
+import com.project.shopping_mall.domain.product.mapper.CategoryMapper;
+import com.project.shopping_mall.domain.product.service.CategoryService;
+import com.project.shopping_mall.globalDto.SingleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+
+@Valid
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+
+    final private CategoryMapper categoryMapper;
+    final private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity postCategory(@RequestBody CategoryDto.Post categoryDto){
+
+        Category category = categoryMapper.categoryDtoToCategory(categoryDto);
+
+        Category savedCategory = categoryService.createPost(category);
+
+        return ResponseEntity.created(URI.create("/category"+ savedCategory.getCategoryId())).build();
+    }
+
+    @PatchMapping("/{category-id}")
+    public ResponseEntity updateCategory(@PathVariable("category-id") @Positive Long categoryId,
+                                          @RequestBody CategoryDto.Patch categoryDto){
+
+        Category category = categoryMapper.categoryPatchDtoToCategory(categoryDto);
+        category.setCategoryId(categoryId);
+
+        Category updateCategory = categoryService.updateCategory(category);
+
+        return new ResponseEntity(new SingleResponseDto<>(updateCategory), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{category-id}")
+    public ResponseEntity deleteCategory(@PathVariable("category-id") @Positive Long categoryId){
+
+        categoryService.deleteCategory(categoryId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+}
