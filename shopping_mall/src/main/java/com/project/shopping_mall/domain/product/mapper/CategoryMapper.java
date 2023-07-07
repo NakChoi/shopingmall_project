99559fc2143ddb58acf8decd35584ec95a073256
NakChoi <nakchoi94@gmail.com>
2023-07-07 23:48:06 +0900

    # feat : Category Product 매퍼 메소드 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/CategoryMapper.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/CategoryMapper.java
@@ -2,8 +2,12 @@ package com.project.shopping_mall.domain.product.mapper;
 
 
 import com.project.shopping_mall.domain.product.dto.CategoryDto;
+import com.project.shopping_mall.domain.product.dto.ProductCategoryDto;
 import com.project.shopping_mall.domain.product.entity.Category;
+import com.project.shopping_mall.domain.product.entity.ProductCategory;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
@@ -13,4 +17,12 @@ public interface CategoryMapper {
     Category categoryPatchDtoToCategory(CategoryDto.Patch dto);
 
     CategoryDto.Response categoryToCategoryResponseDto(Category category);
+
+    ProductCategoryDto.Response productCategoryToProductCategoryDto(ProductCategory productCategory);
+
+    ProductCategory productCategoryDtoToProductCategory(ProductCategoryDto.Post dto);
+
+    ProductCategory productCategoryPatchDtoToProductCategory(ProductCategoryDto.Patch dto);
+
+    List<ProductCategoryDto.Response> categoryProductsToCategoryProductDto(List<ProductCategory> productCategoryList);
 }
