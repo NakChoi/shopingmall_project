915266ac4a24c31d839806bbe495e4a77bfc47de
NakChoi <nakchoi94@gmail.com>
2023-07-07 23:47:48 +0900

    # feat : Category Product Service 메서드 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/service/CategoryService.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/service/CategoryService.java
@@ -2,13 +2,20 @@ package com.project.shopping_mall.domain.product.service;
 
 
 import com.project.shopping_mall.domain.product.entity.Category;
+import com.project.shopping_mall.domain.product.entity.ProductCategory;
 import com.project.shopping_mall.domain.product.repository.CategoryRepository;
+import com.project.shopping_mall.domain.product.repository.ProductCategoryRepository;
 import com.project.shopping_mall.exception.CustomException;
 import com.project.shopping_mall.exception.ExceptionCode;
 import com.project.shopping_mall.utils.CustomBeanUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.print.Pageable;
 
 @Service
 @Transactional
@@ -18,13 +25,24 @@ public class CategoryService {
     final private CategoryRepository categoryRepository;
     final private CustomBeanUtils customBeanUtils;
 
-    public Category createPost(Category category){
+    final private ProductCategoryRepository productCategoryRepository;
+
+    public Category createCategory(Category category){
 
         verifyCategoryName(category.getName());
         Category saveCategory = categoryRepository.save(category);
 
         return saveCategory;
     }
+
+    public ProductCategory createProductCategory(ProductCategory productCategory){
+
+
+         ProductCategory saveProductCategory = productCategoryRepository.save(productCategory);
+
+        return saveProductCategory;
+    }
+
 
     public Category updateCategory(Category category){
         Category verifiedCategory = verifyCategoryId(category.getCategoryId());
@@ -34,10 +52,32 @@ public class CategoryService {
         return verifiedCategory;
     }
 
+
+    public Page<ProductCategory> getProductCategory(Long id, int page, int size){
+
+        Page<ProductCategory> productCategoryPage = productCategoryRepository.findByCategory_CategoryId(id , PageRequest.of(page-1, size, Sort.by("createdAt").ascending()) );
+
+        return productCategoryPage;
+    }
+
+    public ProductCategory updateProductCategory(ProductCategory productCategory){
+        ProductCategory verifiedCategory = verifyProductCategoryId(productCategory.getProductCategoryId());
+
+        customBeanUtils.copyNonNullProperties(productCategory, verifiedCategory);
+
+        return verifiedCategory;
+    }
+
     public void deleteCategory(Long id){
         Category category = verifyCategoryId(id);
 
         categoryRepository.delete(category);
+    }
+
+    public void deleteProductCategory(Long id){
+        ProductCategory productCategory = verifyProductCategoryId(id);
+
+        productCategoryRepository.delete(productCategory);
     }
 
     private void verifyCategoryName(String name){
@@ -50,5 +90,12 @@ public class CategoryService {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new CustomException(ExceptionCode.CATEGORY_NOT_EXIST));
 
         return category;
+    }
+
+    private ProductCategory verifyProductCategoryId(Long id) {
+
+        ProductCategory productCategory = productCategoryRepository.findById(id).orElseThrow(() -> new CustomException(ExceptionCode.CATEGORY_NOT_EXIST));
+
+        return productCategory;
     }
 }
