68ef107d28bab076066f39ed2f0b6937a2b8ecc2
NakChoi <nakchoi94@gmail.com>
2023-07-11 21:40:45 +0900

    feat : ProductCategoryRepository JPQL fetch join 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/repository/ProductCategoryRepository.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/repository/ProductCategoryRepository.java
@@ -1,11 +1,15 @@
 package com.project.shopping_mall.domain.product.repository;
 
 import com.project.shopping_mall.domain.product.entity.ProductCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
-    Page<ProductCategory> findByCategory_CategoryId(Long categoryId, Pageable pageable);
+
+    //@Query("SELECT p FROM ProductCategory p JOIN FETCH p.category JOIN FETCH p.product WHERE p.category.id = ?1")
+    @Query("SELECT p FROM ProductCategory p JOIN FETCH p.product WHERE p.category.id = ?1")
+    List<ProductCategory> findByCategory_CategoryId(Long id);
 }
