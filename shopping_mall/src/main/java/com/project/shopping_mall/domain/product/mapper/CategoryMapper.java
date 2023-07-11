abf4e518fd08ee85e55599bebf33f7a168ca2310
NakChoi <nakchoi94@gmail.com>
2023-07-11 21:42:16 +0900

    refactor : category Mapper 수정

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/CategoryMapper.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/CategoryMapper.java
@@ -6,6 +6,7 @@ import com.project.shopping_mall.domain.product.dto.ProductCategoryDto;
 import com.project.shopping_mall.domain.product.entity.Category;
 import com.project.shopping_mall.domain.product.entity.ProductCategory;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -23,10 +24,15 @@ public interface CategoryMapper {
 
     // Product Category Dto
 
+    @Mapping(source = "category", target = "category.categoryId")
+    @Mapping(source = "product", target = "product.productId")
     ProductCategory productCategoryDtoToProductCategory(ProductCategoryDto.Post dto);
 
     ProductCategory productCategoryPatchDtoToProductCategory(ProductCategoryDto.Patch dto);
 
+
+    @Mapping(source = "category.categoryId", target = "category")
+    @Mapping(source = "product.productId", target = "product")
     ProductCategoryDto.Response productCategoryToProductCategoryDto(ProductCategory productCategory);
 
     List<ProductCategoryDto.Response> categoryProductsToCategoryProductDto(List<ProductCategory> productCategoryList);
