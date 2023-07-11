4e0940ad9398b0b17c30586b6cd94f0f74845e16
NakChoi <nakchoi94@gmail.com>
2023-07-11 21:41:53 +0900

    refactor : Product Mapper 수정

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/ProductMapper.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/mapper/ProductMapper.java
@@ -5,6 +5,8 @@ import com.project.shopping_mall.domain.product.dto.ProductDto;
 import com.project.shopping_mall.domain.product.entity.Product;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
@@ -13,6 +15,10 @@ public interface ProductMapper {
     Product productPatchDtoToProduct(ProductDto.Patch productPatchDto);
 
     ProductDto.Response productToProductResponse(Product product);
+
+
+    List<ProductDto.Response> productsToProductResponse(List<Product> product);
+
 
     ProductDto.PatchResponse productToProductPatchDto(Product product);
 
