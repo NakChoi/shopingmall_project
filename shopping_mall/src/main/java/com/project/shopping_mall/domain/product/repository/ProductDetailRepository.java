7ea7b5cd4d18ae320544f91a7498a8e2382f0eff
NakChoi <nakchoi94@gmail.com>
2023-07-12 17:44:55 +0900

    # feat : Product Detail Repository 추가

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/repository/ProductDetailRepository.java
@@ -0,0 +1,8 @@
+package com.project.shopping_mall.domain.product.repository;
+
+import com.project.shopping_mall.domain.product.entity.ProductDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long> {
+
+}
