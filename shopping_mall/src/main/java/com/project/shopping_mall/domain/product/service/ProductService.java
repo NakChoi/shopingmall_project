f5e3a242adea6b9653f131a36df897544577f375
NakChoi <nakchoi94@gmail.com>
2023-07-13 20:37:04 +0900

    # feat : Product Service Product Size CRUD 구현

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/service/ProductService.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/service/ProductService.java
@@ -3,8 +3,10 @@ package com.project.shopping_mall.domain.product.service;
 
 import com.project.shopping_mall.domain.product.entity.Product;
 import com.project.shopping_mall.domain.product.entity.ProductDetail;
+import com.project.shopping_mall.domain.product.entity.ProductSize;
 import com.project.shopping_mall.domain.product.repository.ProductDetailRepository;
 import com.project.shopping_mall.domain.product.repository.ProductRepository;
+import com.project.shopping_mall.domain.product.repository.ProductSizeRepository;
 import com.project.shopping_mall.exception.CustomException;
 import com.project.shopping_mall.exception.ExceptionCode;
 import com.project.shopping_mall.utils.CustomBeanUtils;
@@ -12,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -20,6 +22,7 @@ import java.util.Optional;
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductDetailRepository  productDetailRepository;
+    private final ProductSizeRepository productSizeRepository;
     private final CustomBeanUtils customBeanUtils;
 
     public Product createProduct(Product product){
@@ -40,11 +43,34 @@ public class ProductService {
         return saveProductDetail;
     }
 
+    public ProductSize createProductSize(ProductSize productSize){
+
+        verifyProductById(productSize.getProduct().getProductId());
+
+        ProductSize saveProductSize = productSizeRepository.save(productSize);
+
+        return saveProductSize;
+    }
+
     public Product findProduct (Long productId){
 
         Product product = productRepository.findById(productId).orElseThrow(() -> new CustomException(ExceptionCode.PRODUCT_NOT_FOUND));
 
         return product;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductSize> findProductSize(Long productId){
+
+        verifyProductById(productId);
+
+        List<ProductSize> productSizeList = productSizeRepository.findByProduct_ProductId(productId);
+        System.out.println("==========================================================");
+        if(productSizeList.isEmpty()){
+            throw new CustomException(ExceptionCode.PRODUCT_SIZE_NOT_FOUND);
+        }
+
+        return productSizeList;
     }
 
     public Product updateProduct(Product product) {
@@ -67,11 +93,27 @@ public class ProductService {
         return updateProductDetail;
     }
 
+
+    public ProductSize updateProductSize(ProductSize productSize){
+        ProductSize updateProductSize = verifyProductSizeById(productSize.getProductSizeId());
+
+        customBeanUtils.copyNonNullProperties(productSize, updateProductSize);
+
+        return updateProductSize;
+    }
+
     public void deleteProduct(Long productId){
         Product product = verifyProductById(productId);
 
         productRepository.delete(product);
     }
+
+    public void deleteProductSize(Long productSizeId) {
+        ProductSize productSize = verifyProductSizeById(productSizeId);
+
+        productSizeRepository.delete(productSize);
+    }
+
 
     public void deleteProductDetail(Long productDetailId){
         ProductDetail productDetail = verifyProductDetailById(productDetailId);
@@ -96,5 +138,9 @@ public class ProductService {
 
     private ProductDetail verifyProductDetailById(Long id){
         return productDetailRepository.findById(id).orElseThrow(() -> new CustomException(ExceptionCode.PRODUCT_DETAIL_NOT_FOUND));
+    }
+
+    private ProductSize verifyProductSizeById(Long id) {
+        return productSizeRepository.findById(id).orElseThrow(() -> new CustomException(ExceptionCode.PRODUCT_SIZE_NOT_FOUND));
     }
 }
