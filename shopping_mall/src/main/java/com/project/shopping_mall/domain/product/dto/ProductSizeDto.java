a7ec5e16bbb4582e2fcea9a8290bd310ddd8466a
NakChoi <nakchoi94@gmail.com>
2023-07-13 20:36:07 +0900

    # feat : Product Size Dto 구현

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/product/dto/ProductSizeDto.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/dto/ProductSizeDto.java
@@ -5,31 +5,38 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-
-public class ProductCategoryDto {
-
+public class ProductSizeDto {
 
     @Getter
     @Setter
     public static class Post{
 
+
         private Long product;
 
-        private Long category;
+        private String prod_size;
+
     }
 
+
     @Getter
+    @Setter
     public static class Patch{
-        private Long productId;
 
-        private Long categoryId;
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public static class Response{
         private Long product;
 
-        private Long category;
+        private String prod_size;
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class Response{
+
+        private Long product;
+
+        private String prod_size;
+
     }
 }
