b4fce4b56c62cf5ecc2f899d0aae4fb705bc8094
NakChoi <nakchoi94@gmail.com>
2023-07-15 20:18:13 +0900

    # feat : Product Review Dto 구현

---
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/product/dto/ProductReviewDto.java
@@ -0,0 +1,48 @@
+package com.project.shopping_mall.domain.product.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class ProductReviewDto {
+
+
+    @Getter
+    @Setter
+    public static class Post{
+
+        private Long memberId;
+
+        private Long productId;
+
+        private String title;
+
+        private String content;
+
+        private Integer rating;
+    }
+
+    @Getter
+    @Setter
+    public static class Patch{
+
+        private String title;
+
+        private String content;
+
+        private Integer rating;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response{
+
+        private String title;
+
+        private String content;
+
+        private Integer rating;
+    }
+}
