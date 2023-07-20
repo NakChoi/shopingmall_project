9e01f4f0e49a24748f4aca1f2a98e5ed63ca9252
NakChoi <nakchoi94@gmail.com>
2023-07-20 22:36:13 +0900

    # feat : Coupon Dto Post, Patch, Response Class 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/dto/CouponDto.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/dto/CouponDto.java
@@ -1,16 +1,54 @@
 package com.project.shopping_mall.domain.coupon.dto;
 
+import com.project.shopping_mall.domain.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 public class CouponDto {
 
+    @Getter
+    @Setter
     public static class Post{
 
+        private String title;
+
+        private String description;
+
+        private Integer discount;
+
+        private Integer amount;
+
+        private Member member;
+
     }
 
+
+    @Getter
+    @Setter
     public static class Patch{
 
+        private String title;
+
+        private String description;
+
+        private Integer discount;
+
     }
 
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
     public static class Response{
+
+        private String title;
+
+        private String description;
+
+        private Integer discount;
+
+        private Integer amount;
 
     }
 }
