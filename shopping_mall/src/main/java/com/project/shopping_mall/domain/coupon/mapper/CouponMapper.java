472a763de1f3987bbcb65bb291435b978d3b79db
NakChoi <nakchoi94@gmail.com>
2023-07-20 22:35:52 +0900

    # feat : Coupon Mapper 메서드 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/mapper/CouponMapper.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/mapper/CouponMapper.java
@@ -1,8 +1,22 @@
 package com.project.shopping_mall.domain.coupon.mapper;
 
 
+import com.project.shopping_mall.domain.coupon.dto.CouponDto;
+import com.project.shopping_mall.domain.coupon.entity.Coupon;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CouponMapper {
+
+    @Mapping(source = "member", target = "member.memberId")
+    Coupon couponPostDtoToCoupon(CouponDto.Post couponPostDto);
+
+    Coupon couponPatchDtoToCoupon(CouponDto.Patch couponPostDto);
+
+    CouponDto.Response couponToCouponResponseDto(Coupon coupon);
+
+    List<CouponDto.Response> couponToCouponResponseDto(List<Coupon> coupon);
 }
