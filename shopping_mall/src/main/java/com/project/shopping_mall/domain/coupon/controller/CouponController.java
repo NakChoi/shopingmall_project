b3947b5e6c70f9cd21747cec4e9a6e8accc9be5f
NakChoi <nakchoi94@gmail.com>
2023-07-20 22:36:28 +0900

    # feat : Coupon Controller 틀 구현

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/controller/CouponController.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/controller/CouponController.java
@@ -2,11 +2,36 @@ package com.project.shopping_mall.domain.coupon.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/coupons")
 public class CouponController {
+
+
+    @PostMapping
+    public ResponseEntity postCoupon(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity patchCoupon(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping           // 쿠폰을 단일로 가져올 일은 없지 않을까?
+    public ResponseEntity getCoupons(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteCoupon(){
+
+        return ResponseEntity.ok().build();
+    }
 }
