e80625ede0f3f10b213ba09c1bd7e409f9f26789
NakChoi <nakchoi94@gmail.com>
2023-07-21 13:55:29 +0900

    # feat : Post Repository findByTitle 메서드 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/repository/CouponRepository.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/repository/CouponRepository.java
@@ -3,5 +3,9 @@ package com.project.shopping_mall.domain.coupon.repository;
 import com.project.shopping_mall.domain.coupon.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    Optional<Coupon> findByTitle(String name);
 }
