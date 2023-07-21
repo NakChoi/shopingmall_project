80e45deaeb816aeb28d3b94178c1b532f78ff8ef
NakChoi <nakchoi94@gmail.com>
2023-07-21 13:55:42 +0900

    # feat : Post Service postCoupon 메서드 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/service/CouponService.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/domain/coupon/service/CouponService.java
@@ -1,4 +1,35 @@
 package com.project.shopping_mall.domain.coupon.service;
 
+import com.project.shopping_mall.domain.coupon.entity.Coupon;
+import com.project.shopping_mall.domain.coupon.repository.CouponRepository;
+import com.project.shopping_mall.exception.CustomException;
+import com.project.shopping_mall.exception.ExceptionCode;
+import com.project.shopping_mall.utils.CustomBeanUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    private final CustomBeanUtils customBeanUtils;
+
+    public Coupon postCoupon(Coupon coupon){
+        verifyCouponByTitle(coupon); // title 이 일련 번호로도 쓰인다. 타이틀로 중복 검사.
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+
+        return savedCoupon;
+    }
+
+    private void verifyCouponByTitle(Coupon coupon){
+        couponRepository.findByTitle(coupon.getTitle()).ifPresent( verifiedCoupon -> {
+            throw new CustomException(ExceptionCode.COUPON_EXIST);
+        });
+    }
+
 }
