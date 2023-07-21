4e2421c2f0d77f86673d3dbf669d74f5809e7473
NakChoi <nakchoi94@gmail.com>
2023-07-21 14:01:26 +0900

    # feat : Exception Code Coupon Exists 추가

--- shopping_mall/src/main/java/com/project/shopping_mall/exception/ExceptionCode.java
+++ shopping_mall/src/main/java/com/project/shopping_mall/exception/ExceptionCode.java
@@ -6,16 +6,17 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not Found"),
     MEMBER_EXIST(HttpStatus.CONFLICT, "Member Exist"),
-
     PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "Password is Not Matched"),
     ACCESSTOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "AceessToken is Expired"),
-
     PRODUCT_NAME_EXIST(HttpStatus.CONFLICT, "Product Name Exists"),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product Not Found"),
-
-    PRODUCT_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "Product Detail NOt Found"),
+    PRODUCT_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "Product Detail Not Found"),
+    PRODUCT_SIZE_NOT_FOUND(HttpStatus.NOT_FOUND, "Product Size Not Found"),
+    PRODUCT_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "Product Image Not Found"),
     CATEGORY_NAME_EXIST(HttpStatus.CONFLICT, "Category Name Exists"),
     CATEGORY_NOT_EXIST(HttpStatus.CONFLICT, "Category Not Exists"),
+    REVIEW_NOT_EXIST(HttpStatus.CONFLICT, "Review Not Exists"),
+    COUPON_EXIST(HttpStatus.CONFLICT, "Coupon Exist"),
     REFRESHTOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "Refresh Token is Expired");
 
 
